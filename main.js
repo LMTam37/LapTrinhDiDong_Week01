e39e09431aa6b55177a904cfb072b7f34ee53ceb
MinhTam <lait2553@gmail.com>
2023-09-12 20:08:50 +0700

    add: bai 6

--- main.js
+++ main.js
@@ -130,3 +130,17 @@ checkWinner = (dolphin, koalas) => {
 
 console.log(checkWinner(Dolphins1, Koalas1));
 console.log(checkWinner(Dolphins2, Koalas2));
+//bai 6
+console.log("bai 6");
+
+calcTip = (bill) =>{
+    return bill > 50 && bill < 300 ? bill*0.15 : bill*0.2;
+}
+
+bills = [125, 555, 44]
+tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+total = []
+for(i = 0; i < 3; i++){
+    total.push(bill[i] + tips[i]);
+}
+console.log(total);
