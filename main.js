246cbfb57f7da68feea1084220e680137aebd0d9
Minh Tam <lait2553@gmail.com>
2023-09-12 13:40:10 +0700

    refactor: variable name and round result from bai 2

--- main.js
+++ main.js
@@ -8,33 +8,33 @@ markHigherBMI = function(o1, o2) {
     return bmi(o1) > bmi(o2);
 }
 
-Mark = {
+Mark1 = {
     mass: 78,
     height: 1.69
 }
 
-Jhon = {
+Jhon1 = {
     mass: 92,
     height: 1.95
 }
 
-console.log('test data 1:', Mark, Jhon);
+console.log('data 1:', Mark1, Jhon1);
 
-console.log(markHigherBMI(Mark, Jhon));
+console.log(markHigherBMI(Mark1, Jhon1));
 
-Mark = {
+Mark2 = {
     mass: 95,
     height: 1.88
 }
 
-Jhon = {
+Jhon2 = {
     mass: 85,
     height: 1.76
 }
 
-console.log('test data 2:', Mark, Jhon);
+console.log('data 2:', Mark2, Jhon2);
 
-console.log(markHigherBMI(Mark, Jhon));
+console.log(markHigherBMI(Mark2, Jhon2));
 //bai 2
 console.log("bai 2");
 printResult = (o1, o2) => {
@@ -49,25 +49,16 @@ printResult = (o1, o2) => {
 printResultWithBmi = (o1, o2) => {
     result = markHigherBMI(o1, o2)
     if (result) {
-        console.log("Mark's BMI " + bmi(o1) + " is higher than John's " + bmi(o2) + " !");
+        console.log("Mark's BMI " + Math.round(bmi(o1) * 10) / 10 + " is higher than John's " + Math.round(bmi(o2) * 10) / 10 + " !");
     } else {
-        console.log("John's BMI " + bmi(o2) + " is higher than Mark's " + bmi(o1) + " !");
+        console.log("John's BMI " + Math.round(bmi(o2) * 10) / 10 + " is higher than Mark's " + Math.round(bmi(o1) * 10) / 10 + " !");
     }
 }
+console.log("data 1");
 
-Mark = {
-    mass: 78,
-    height: 1.69
-}
+printResult(Mark1, Jhon1)
 
-Jhon = {
-    mass: 92,
-    height: 1.95
-}
-
-printResult(Mark, Jhon)
-
-printResultWithBmi(Mark, Jhon)
+printResultWithBmi(Mark1, Jhon1)
 
 Mark = {
     mass: 95,
@@ -78,6 +69,8 @@ Jhon = {
     mass: 85,
     height: 1.76
 }
-printResult(Mark, Jhon)
+console.log("data 2");
 
-printResultWithBmi(Mark, Jhon)
+printResult(Mark2, Jhon2)
+
+printResultWithBmi(Mark2, Jhon2)
