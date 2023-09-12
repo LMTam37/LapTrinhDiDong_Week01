4531bcd95a0d4734277013e8d9ecf565800340d3
Minh Tam <lait2553@gmail.com>
2023-09-12 13:35:21 +0700

    add: bai 2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //bai 1
+console.log("bai 1");
 bmi = function(object) {
     return object.mass / object.height ** 2;
 }
@@ -35,3 +36,48 @@ console.log('test data 2:', Mark, Jhon);
 
 console.log(markHigherBMI(Mark, Jhon));
 //bai 2
+console.log("bai 2");
+printResult = (o1, o2) => {
+    result = markHigherBMI(o1, o2)
+    if (result) {
+        console.log("Mark's BMI is higher than John's!");
+    } else {
+        console.log("John's BMI is higher than Mark's!");
+    }
+}
+
+printResultWithBmi = (o1, o2) => {
+    result = markHigherBMI(o1, o2)
+    if (result) {
+        console.log("Mark's BMI " + bmi(o1) + " is higher than John's " + bmi(o2) + " !");
+    } else {
+        console.log("John's BMI " + bmi(o2) + " is higher than Mark's " + bmi(o1) + " !");
+    }
+}
+
+Mark = {
+    mass: 78,
+    height: 1.69
+}
+
+Jhon = {
+    mass: 92,
+    height: 1.95
+}
+
+printResult(Mark, Jhon)
+
+printResultWithBmi(Mark, Jhon)
+
+Mark = {
+    mass: 95,
+    height: 1.88
+}
+
+Jhon = {
+    mass: 85,
+    height: 1.76
+}
+printResult(Mark, Jhon)
+
+printResultWithBmi(Mark, Jhon)
